9aac3380c8ad3de20ea3c7ffcadb2a61b49d5391
e1b22027 <e1b22027@oit.ac.jp>
2024-10-08 16:08:03 +0900

    入室処理をDIとnew2種類の方法で実装

--- src/main/java/oit/is/lec03/kadai/lec03/controller/Sample31Controller.java
+++ src/main/java/oit/is/lec03/kadai/lec03/controller/Sample31Controller.java
@@ -10,12 +10,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.lec03.kadai.lec03.model.Room;
+
 /**
  * /sample3へのリクエストを扱うクラス authenticateの設定をしていれば， /sample3へのアクセスはすべて認証が必要になる
  */
 @Controller
 @RequestMapping("/sample3")
 public class Sample31Controller {
+
+  @Autowired
+  private Room room;
 
   @GetMapping("step1")
   public String sample31() {
@@ -45,4 +50,24 @@ public class Sample31Controller {
   public String sample37() {
     return "sample37.html";
   }
+
+  @GetMapping("step8")
+  public String sample38(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model.addAttribute("room", this.room);
+
+    return "sample37.html";
+  }
+
+  @GetMapping("step9")
+  public String sample39(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    Room newRoom = new Room();
+    newRoom.addUser(loginUser);
+    model.addAttribute("new_room", newRoom);
+
+    return "sample37.html";
+  }
+
 }
